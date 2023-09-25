ace0c88e95a58bd8f31fbcea68a82f5be147a294
Yoeri-z <yoeriz123@gmail.com>
2023-09-25 19:09:20 +0200

    initial files.

--- minimalist_state_flutter/lib/main.dart
+++ minimalist_state_flutter/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class StoreProvider extends InheritedWidget{
   final Store store;
-  StoreProvider({super.key, })
+  StoreProvider({super.key, required this.store, required Widget child})
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) {
     // TODO: implement updateShouldNotify
