1b1d7f27ff695e5b0d24415950148efba6dd0fd2
Yoeri-z <yoeriz123@gmail.com>
2023-09-26 15:07:23 +0200

    inherited widget added

--- minimalist_state_flutter/lib/main.dart
+++ minimalist_state_flutter/lib/main.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
 
-class StoreProvider extends InheritedWidget{
-  final Store store;
-  StoreProvider({super.key, required this.store, required Widget child})
+import 'package:minimalist_state/minimalist_state.dart' as min;
+
+class StoreProvider<T> extends InheritedWidget {
+  final min.Store store;
+  const StoreProvider({super.key, required this.store, required Widget child})
+      : super(child: child);
+
   @override
-  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
-    // TODO: implement updateShouldNotify
-    throw UnimplementedError();
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
+}
+
+class StoreListener<T extends min.State> extends StatefulWidget {
+  const StoreListener(
+      {super.key,
+      required this.store,
+      required this.onChange,
+      required this.child});
+  final min.Store store;
+  final VoidCallbackAction onChange;
+  final Widget child;
+  @override
+  State<StoreListener> createState() => _StoreListenerState<T>();
+}
+
+class _StoreListenerState<T extends min.State> extends State<StoreListener> {
+  late final listener;
+
+  @override
+  void initState() {
+    super.initState();
+    listener = min.Listener<T>(
+        onEvent: (state) {
+          widget.onChange;
+          setState(() {});
+        },
+        store: widget.store);
   }
 
-
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
 }
